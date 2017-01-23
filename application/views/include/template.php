97bad40e085ebd84604dea2fd59541496f86ac19
PXgamer <owzie123@gmail.com>
2017-01-23 12:35:44 +0000

    Fixed title tag

--- application/views/include/template.php
+++ application/views/include/template.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Cwtch Potato</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
     <link href="/assets/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
